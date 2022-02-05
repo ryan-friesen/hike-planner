d8a0fdbdc82c040238704ca57413d8a61b04cc92
Ryan Friesen <viniloruido@gmail.com>
2022-02-04 19:48:27 -0800

    log - push test

--- src/components/PlannedHike/index.js
+++ src/components/PlannedHike/index.js
@@ -1,6 +1,7 @@
 import styles from './styles.module.css';
 
 const PlannedHike = (props) => {
+  console.log(props);
   return <div className={`${styles.containerBorder}`}>TEST PLANNED HIKE</div>;
 };
 
